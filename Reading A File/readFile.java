5d325948a5409f1bfc5c81617dad95b623bd9a4a
AlparslanDuru <81374685+AlparslanDuru@users.noreply.github.com>
2023-01-09 12:07:29 -0600

    Reading the File.txt

---
+++ Reading A File/readFile.java
@@ -0,0 +1,20 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
+class Main {
+  
+  public static void main(String[] args) throws IOException{
+    ReadTheText();
+  }
+  
+  public static void ReadTheText() throws IOException{
+    FileInputStream in = new FileInputStream("File.txt");
+    
+    int k=0;
+    
+    while ((k = in.read()) != -1){
+      System.out.print((char)k);
+    }
+  }
+  
+}
